d03c05496c9f89f46d9bcfb052eee837982d518b
Heinz Riener <heinz.riener@gmail.com>
2018-06-07 11:29:15 +0200

    function command.

--- src/cli/commands/function.hpp
+++ src/cli/commands/function.hpp
@@ -24,6 +24,8 @@
  */
 
 #include <alice/alice.hpp>
+#include <random>
+#include <chrono>
 
 namespace alice
 {
@@ -34,40 +36,98 @@ public:
   explicit function_command( const environment::ptr& env )
     : command( env, "loads an incompletely-specified Boolean function and adds it to the store" )
   {
-    opts.add_option( "string", f, "Incompletely-specified Boolean function (MSB ... LSB)" );
+    opts.add_option( "--num,-n",        num,        "Specifies how many Boolean functions are generated (default: 1)" );
+    opts.add_option( "--truthtable,-t", tt,         "Truth table (in binary) of Boolean function (MSB ... LSB)" );
+    opts.add_option( "--random,-r",     variables,  "Random completely-specified Boolean function.  As parameter the number of Boolean variables has to be passed." );
   }
 
 protected:
   void execute()
   {
-    const unsigned number_of_variables = std::ceil( std::log2( f.size() ) );
-    std::reverse( f.begin(), f.end() );
-
-    kitty::dynamic_truth_table bits( number_of_variables );
-    kitty::dynamic_truth_table care( number_of_variables );
-
-    unsigned i;
-    for ( i = 0; i < f.size(); ++i )
+    if ( tt != "" )
     {
-      if ( f[i] == '1' )
+      const unsigned number_of_variables = std::ceil( std::log2( tt.size() ) );
+
+      std::reverse( tt.begin(), tt.end() );
+
+      kitty::dynamic_truth_table bits( number_of_variables );
+      kitty::dynamic_truth_table care( number_of_variables );
+
+      unsigned i;
+      for ( i = 0; i < tt.size(); ++i )
       {
-        kitty::set_bit( bits, i );
-        kitty::set_bit( care, i );
+        if ( tt[ i ] != '1' && tt[ i ] != '0' && tt[ i ] != '-' )
+        {
+          std::cout << fmt::format( "[w] unsupported character `{}` treated as don't care\n", tt[ i ] );
+        }
+
+        if ( tt[ i ] == '1' )
+        {
+          kitty::set_bit( bits, i );
+          kitty::set_bit( care, i );
+        }
+        else if ( tt[ i ] == '0' )
+        {
+          kitty::clear_bit( bits, i );
+          kitty::set_bit(   care, i );
+        }
       }
-      else if ( f[i] == '0' )
+
+      for ( auto j = 0; j < num; ++j )
       {
-        kitty::clear_bit( bits, i );
-        kitty::set_bit(   care, i );
+        env->store<function_storee>().extend() = function_storee{bits, care, number_of_variables};
       }
     }
+    else
+    {
+      assert( variables != 0 );
 
-    env->store<function_storee>().extend() = function_storee{bits, care, number_of_variables};
+      std::default_random_engine random_engine(std::chrono::steady_clock::now().time_since_epoch().count());
+      std::uniform_int_distribution<> dist( 0, 100 );
+
+      const auto len = 1ul << variables;
+      for ( auto j = 0; j < num; ++j )
+      {
+        kitty::dynamic_truth_table bits( variables );
+        kitty::dynamic_truth_table care( variables );
+
+        /* bits */
+        for ( auto i = 0ul; i < len; ++i )
+        {
+          if ( dist( random_engine ) > 50 )
+          {
+            set_bit( bits, i );
+          }
+          else
+          {
+            clear_bit( bits, i );
+          }
+        }
+
+        /* care */
+        for ( auto i = 0ul; i < len; ++i )
+        {
+          if ( dist( random_engine ) > 50 )
+          {
+            set_bit( care, i );
+          }
+          else
+          {
+            clear_bit( care, i );
+          }
+        }
+
+        env->store<function_storee>().extend() = function_storee{bits, care, variables};
+      }
+    }
   }
 
 private:
-  std::string f = "";
-};
- 
+  unsigned num = 1u;
+  std::string tt = "";
+  unsigned variables = 0u;
+}; /* function */
+
 } // namespace alice
 
 // Local Variables:
