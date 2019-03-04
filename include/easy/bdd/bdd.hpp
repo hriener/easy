4f85af6c685e098359148906cbb9359ccaf2a2d0
Heinz Riener <heinz.riener@gmail.com>
2019-03-04 11:14:47 +0100

    bdd.

--- include/easy/bdd/bdd.hpp
+++ include/easy/bdd/bdd.hpp
@@ -35,199 +35,170 @@
 #include <vector>
 #include <stack>
 #include <unordered_map>
+#include <iostream>
 
 namespace easy
 {
 
+class bdd_function
+{
+public:
+  explicit bdd_function( uint32_t node, bool complemented )
+    : cmpl( complemented )
+    , node( node )
+  {
+  }
+
+  bool operator==( bdd_function const& other ) const
+  {
+    return value == other.value;
+  }
+
+  bool operator!=( bdd_function const& other ) const
+  {
+    return this->operator==( other );
+  }
+
+  bdd_function operator!() const
+  {
+    return bdd_function{node, !cmpl};
+  }
+
+public:
+  union
+  {
+    uint32_t value;
+    struct
+    {
+      uint32_t cmpl :  1;
+      uint32_t node : 19;
+    };
+  };
+}; /* bdd_function */
+
+class bdd_node
+{
+public:
+  explicit bdd_node( uint32_t var, bdd_function lo, bdd_function hi )
+    : var( var )
+    , ref( 0 )
+    , lo( lo.value )
+    , hi( hi.value )
+  {
+  }
+
+  bdd_function get_lo() const
+  {
+    return bdd_function{ uint32_t( lo ) >> 1u, bool( lo & 1u ) };
+  }
+
+  bdd_function get_hi() const
+  {
+    return bdd_function{ uint32_t( hi ) >> 1u, bool( hi & 1u ) };
+  }
+
+public:
+  union
+  {
+    uint64_t value;
+    struct
+    {
+      uint64_t var : 12;
+      uint64_t ref : 12;
+      uint64_t lo  : 20; /* else */
+      uint64_t hi  : 20; /* then */
+    };
+  };
+}; /* bdd_node */
+
 class BDD
 {
+public:
+  using index_t = uint32_t;
+
 private:
-  struct node_t
-  {
-    node_t( uint32_t var, uint32_t lo, uint32_t hi )
-      : var( var )
-      , ref( 0 )
-      , dead( 0 )
-      , lo( lo )
-      , hi( hi )
-    {}
-
-    uint64_t var : 12;
-    uint64_t ref : 11;
-    uint64_t dead : 1;
-    uint64_t lo : 20;
-    uint64_t hi : 20;
-  }; /* node_t */
-
   enum op_t
   {
     bdd_and
   , bdd_xor
   };
 
-private:
   struct unique_table_hash
   {
-    std::size_t operator()( std::pair<uint32_t, uint32_t> const& p ) const
+    std::size_t operator()( std::pair<bdd_function, bdd_function> const& p ) const
     {
-      return 12582917 * p.first + 4256249 * p.second;
+      return 12582917 * p.first.value + 4256249 * p.second.value;
     }
   }; /* unique_table_hash */
 
   struct compute_table_hash
   {
-    std::size_t operator()( std::tuple<uint32_t, uint32_t, op_t> const& p ) const
+    std::size_t operator()( std::tuple<bdd_function, bdd_function, op_t> const& p ) const
     {
-      return 12582917 * std::get<0>( p ) + 4256249 * std::get<1>( p ) + 741457 * static_cast<uint32_t>( std::get<2>( p ) );
+      return 12582917 * std::get<0>( p ).value + 4256249 * std::get<1>( p ).value + 741457 * static_cast<uint32_t>( std::get<2>( p ) );
     }
   }; /* compute_table_hash */
 
 public:
-  using node = uint32_t;
-    
-  explicit BDD( uint32_t num_vars, uint32_t log_num_objs = 16 )
+  explicit BDD( uint32_t num_vars, uint32_t size )
     : unique_table( num_vars )
   {
-    nodes.reserve( 1u << log_num_objs );
-
-    nodes.emplace_back( num_vars, 0u, 0u );
-    nodes.emplace_back( num_vars, 1u, 1u );
+    nodes.reserve( 1u << size );
+    nodes.emplace_back( num_vars, bot(), bot() );
 
     for ( auto v = 0u; v < num_vars; ++v )
-      ref( unique( v, 0, 1 ) );
-  }
-
-  node bot() const
-  {
-    return 0u;
-  }
-
-  node top() const
-  {
-    return 1u;
-  }
-
-  node get_ith_var( uint32_t const i ) const
-  {
-    return 2u + i;
-  }
-
-  void ref( node const& n )
-  {
-    if ( n > 1u )
     {
-      ++(nodes[n].ref);
+      ref( unique( v, bot(), top() ) );
     }
   }
 
-  void deref( node const& n )
+  bdd_function bot() const
   {
-    if ( n > 1u && nodes[n].ref > 0u )
+    return bdd_function{0u, false};
+  }
+
+  bdd_function top() const
+  {
+    return bdd_function{0u, true};
+  }
+
+  bdd_function ith_var( uint32_t const i ) const
+  {
+    return bdd_function{1u + i, false};
+  }
+
+  void ref( index_t const& index )
+  {
+    if ( index > 0u )
     {
-      --(nodes[n].ref);
+      ++( nodes[index].ref );
     }
+  }
+
+  void ref( bdd_function const& f )
+  {
+    ref( f.node );
+  }
+
+  void deref( index_t const& index )
+  {
+    if ( index > 0u && nodes[index].ref > 0u )
+    {
+      --( nodes[index].ref );
+    }
+  }
+
+  void deref( bdd_function const& f )
+  {
+    deref( f.node );
   }
 
   uint64_t num_nodes() const
   {
-    return nodes.size() - 2u - free.size();
+    return nodes.size() - 1u;
   }
 
-  node compute_and( node f, node g )
-  {
-    /* trivial cases */
-    if ( f == 0 || g == 0 ) return 0;
-    if ( f == 1 ) return g;
-    if ( g == 1 ) return f;
-    if ( f == g ) return f;
-    if ( f == !f ) return 0;
-    
-    if ( f > g )
-    {
-      std::swap( f, g );
-    }
-    
-    /* lookup in computed cache */
-    auto const it = compute_table.find( {f, g, bdd_and} );
-    if ( it != compute_table.end() )
-    {
-      assert( !nodes[it->second].dead );
-      return it->second;
-    }
-
-    auto const& F = nodes[f];
-    auto const& G = nodes[g];
-
-    node r_lo, r_hi;
-    if ( F.var < G.var )
-    {
-      r_lo = compute_and( F.lo, g );
-      r_hi = compute_and( F.hi, g );
-    }
-    else if ( F.var > G.var )
-    {
-      r_lo = compute_and( f, G.lo );
-      r_hi = compute_and( f, G.hi );
-    }
-    else
-    {
-      r_lo = compute_and( F.lo, G.lo );
-      r_hi = compute_and( F.hi, G.hi );
-    }
-
-    auto const var = std::min( F.var, G.var );
-    return compute_table[{f,g,bdd_and}] = unique( var, r_lo, r_hi );      
-  }
-
-  node compute_xor( node f, node g )
-  {
-    /* trivial cases */
-    if ( f == 1 ) return !g;
-    if ( g == 1 ) return !f;
-    if ( f == 0 ) return g;
-    if ( g == 0 ) return f;
-    if ( f == g ) return 0;
-    if ( f == !g ) return 1;
-    
-    if ( f > g )
-    {
-      std::swap( f, g );
-    }
-    
-    /* lookup in computed cache */
-    auto const it = compute_table.find( {f, g, bdd_and} );
-    if ( it != compute_table.end() )
-    {
-      assert( !nodes[it->second].dead );
-      return it->second;
-    }
-
-    auto const& F = nodes[f];
-    auto const& G = nodes[g];
-
-    node r_lo, r_hi;
-    if ( F.var < G.var )
-    {
-      r_lo = compute_xor( F.lo, g );
-      r_hi = compute_xor( F.hi, g );
-    }
-    else if ( F.var > G.var )
-    {
-      r_lo = compute_xor( f, G.lo );
-      r_hi = compute_xor( f, G.hi );
-    }
-    else
-    {
-      r_lo = compute_xor( F.lo, G.lo );
-      r_hi = compute_xor( F.hi, G.hi );
-    }
-
-    auto const var = std::min( F.var, G.var );
-    return compute_table[{f,g,bdd_and}] = unique( var, r_lo, r_hi );      
-  }
-
-private:
-  node unique( uint32_t var, uint32_t lo, uint32_t hi )
+  bdd_function unique( uint32_t var, bdd_function lo, bdd_function hi )
   {
     /* BDD reduction rules */
     if ( lo == hi )
@@ -235,28 +206,31 @@ private:
       return lo;
     }
 
-    assert( nodes[lo].var > var );
-    assert( nodes[hi].var > var );
-    
-    /* unique table lookup */
+    assert( nodes[lo.node].var > var );
+    assert( nodes[hi.node].var > var );
+
+    /* normalize complemented ELSE-edges */
+    bool complemented;
+    if ( lo.cmpl )
+    {
+      complemented = true;
+      lo.cmpl = false;
+      hi.cmpl = !hi.cmpl;
+    }
+    else
+    {
+      complemented = false;
+    }
+
+    /* look-up in unique table */
     auto const it = unique_table[var].find( {lo,hi} );
     if ( it != unique_table[var].end() )
     {
-      assert( !nodes[it->second].dead );
-      return it->second;
+      return bdd_function{it->second, complemented};
     }
 
-    /* create new node */
-    node r;
-
-    if ( !free.empty() )
-    {
-      r = free.top();
-      free.pop();
-      nodes[r].ref = nodes[r].dead = 0;
-      nodes[r] = {var,lo,hi};
-    }
-    else if ( nodes.size() < nodes.capacity() )
+    index_t r;
+    if ( nodes.size() < nodes.capacity() )
     {
       r = nodes.size();
       nodes.emplace_back( var, lo, hi );
@@ -267,24 +241,146 @@ private:
       exit( 1 );
     }
 
-    /* increase ref counts */
-    if ( lo > 1 )
-    {
-      ++(nodes[lo].ref);
-    }
-    else if ( hi > 1 )
-    {
-      ++(nodes[hi].ref);
-    }
-    return unique_table[var][{lo,hi}] = r;
+    ref( lo.node );
+    ref( hi.node );
+
+    unique_table[var][{lo,hi}] = r;
+    return bdd_function{r, complemented};
   }
-  
-private:
-  std::vector<node_t> nodes;
-  std::stack<node> free;
-  std::vector<std::unordered_map<std::pair<uint32_t, uint32_t>, node, unique_table_hash>> unique_table;
-  std::unordered_map<std::tuple<uint32_t, uint32_t, op_t>, node, compute_table_hash> compute_table;
-}; /* bdd_base */
+
+  bdd_function create_and( bdd_function f, bdd_function g )
+  {
+    /* trivial cases */
+    if ( f == bot() || g == bot() ) return bot();
+    if ( f == top() ) return g;
+    if ( g == top() ) return f;
+    if ( f == g ) return f;
+    if ( f == !f ) return bot();
+
+    if ( f.node > g.node )
+    {
+      std::swap( f, g );
+    }
+
+    /* normalize complemented ELSE-edges */
+    bool complemented;
+    if ( f.cmpl )
+    {
+      complemented = true;
+      f.cmpl = !f.cmpl;
+      g.cmpl = false;
+    }
+    else
+    {
+      complemented = false;
+    }
+
+    /* look-up in computed cache */
+    auto const it = compute_table.find( {f, g, bdd_and} );
+    if ( it != compute_table.end() )
+    {
+      return bdd_function{it->second, complemented};
+    }
+
+    auto const& F = nodes[f.node];
+    auto const& G = nodes[g.node];
+
+    bdd_function lo = bot(), hi = bot();
+    if ( F.var < G.var )
+    {
+      lo = create_and( F.get_lo(), g );
+      hi = create_and( F.get_hi(), g );
+    }
+    else if ( F.var > G.var )
+    {
+      lo = create_and( f, G.get_lo() );
+      hi = create_and( f, G.get_hi() );
+    }
+    else
+    {
+      lo = create_and( F.get_lo(), G.get_lo() );
+      hi = create_and( F.get_hi(), G.get_hi() );
+    }
+
+    auto const var = std::min( F.var, G.var );
+
+    auto h = unique( var, lo, hi );
+
+    compute_table[{f, g, bdd_and}] = h.node;
+    h.cmpl = h.cmpl ^ complemented;
+    return h;
+  }
+
+  bdd_function create_xor( bdd_function f, bdd_function g )
+  {
+    /* trivial cases */
+    if ( f == top() ) return !g;
+    if ( g == top() ) return !f;
+    if ( f == bot() ) return g;
+    if ( g == bot() ) return f;
+
+    if ( f == g ) return bot();
+    if ( f == !g ) return top();
+
+    if ( f.node > g.node )
+    {
+      std::swap( f, g );
+    }
+
+    /* normalize complemented EDSE-edges */
+    bool complemented;
+    if ( f.cmpl )
+    {
+      complemented = true;
+      f.cmpl = !f.cmpl;
+      g.cmpl = false;
+    }
+    else
+    {
+      complemented = false;
+    }
+
+    /* look-up in computed cache */
+    auto const it = compute_table.find( {f, g, bdd_xor} );
+    if ( it != compute_table.end() )
+    {
+      return bdd_function{it->second, complemented};
+    }
+
+    auto const& F = nodes[f.node];
+    auto const& G = nodes[g.node];
+
+    bdd_function lo = bot(), hi = bot();
+    if ( F.var < G.var )
+    {
+      lo = create_xor( F.get_lo(), g );
+      hi = create_xor( F.get_hi(), g );
+    }
+    else if ( F.var > G.var )
+    {
+      lo = create_xor( f, G.get_lo() );
+      hi = create_xor( f, G.get_hi() );
+    }
+    else
+    {
+      lo = create_xor( F.get_lo(), G.get_lo() );
+      hi = create_xor( F.get_hi(), G.get_hi() );
+    }
+
+    auto const var = std::min( F.var, G.var );
+
+    auto h = unique( var, lo, hi );
+
+    compute_table[{f, g, bdd_xor}] = h.node;
+    h.cmpl = h.cmpl ^ complemented;
+    return h;
+  }
+
+protected:
+  std::vector<bdd_node> nodes;
+  std::vector<std::unordered_map<std::pair<bdd_function,bdd_function>, index_t, unique_table_hash>> unique_table;
+  std::unordered_map<std::tuple<bdd_function, bdd_function, op_t>, index_t, compute_table_hash> compute_table;
+}; /* BDD */
 
 } /* namespace easy */
 
